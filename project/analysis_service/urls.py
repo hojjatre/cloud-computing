f44d91770ec5249de26c1b6acc0a82e13134686c
navid.ag <navid.ag.navid@gmail.com>
2021-12-09 22:39:54 +0330

    Added some components to the analysis.

---
+++ project/analysis_service/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework import views
+from analysis_service import views as app_view
+
+
+urlpatterns = [
+    path('comparison/<str:name_game1>/<str:name_game2>', app_view.ComparisonTwoGames.as_view()),
+    path('totalsales/<int:yearOne>/<int:yearTwo>', app_view.TotalSalesEachYear.as_view()),
+    path('comparisonpublisher/<int:yearOne>/<int:yearTwo>/<str:publisherOne>/<str:publisherTwo>', app_view.TotalSalesBetweenPublisherEachYear.as_view()),
+]
--- project/project/urls.py
+++ project/project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('', include('main_service.urls')),
+    path('', include('analysis_service.urls')),
 ]
